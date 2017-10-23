f1dc523e1233c1157b403b2c516c5139e7f4da51
Dimokritos Stamatakis <dimstamat@gmail.com>
2017-10-23 17:38:29 -0400

    fixing corrupt jpeg issue

--- src/streamclient.cc
+++ src/streamclient.cc
@@ -27,7 +27,7 @@ int main( int argc, char** argv )
     #endif
 
     UDPClient client = UDPClient("dimosdroid.zapto.org", 9090);
-    client.send((unsigned char*) ACCEPTANCE_KEY, strlen(ACCEPTANCE_KEY));
+	client.send((unsigned char*) ACCEPTANCE_KEY, strlen(ACCEPTANCE_KEY));
 	namedWindow( "Fish tank", WINDOW_AUTOSIZE ); // Create a window for display.
     while(1){
 		#if SHOW_FPS
@@ -35,20 +35,21 @@ int main( int argc, char** argv )
     	#endif
 
 		buf = client.receive(len);
+		cout "Received "<< len<<endl;
 		Mat imgenc = Mat(640, 480, CV_8UC3, (unsigned char*)buf);
 		Mat img = imdecode(imgenc, IMREAD_COLOR);
 		if( img.empty() ) {                     // Check for invalid input
         	cout <<  "Could not create image" << std::endl ;
         	return -1;
     	}
-    	imshow( "Display window", img );                // Show our image inside it.
+    	imshow("Fish tank", img );                // Show our image inside it.
 		#if SHOW_FPS
        	gettimeofday(&frameEnd, NULL);
        	long int diff = (frameEnd.tv_sec - frameStart.tv_sec)*1000 + (frameEnd.tv_usec - frameStart.tv_usec)/1000;
        	int fps = 1000/diff;
        	printf("fps: %d\n", fps);
        	#endif
-		waitKey(1); // Wait for a keystroke in the window
+		waitKey(10); // Wait for a keystroke in the window
     }
     return 0;
 }
